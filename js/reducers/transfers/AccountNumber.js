441b197e9238b19c887ac55c73865034b599c826
choco <tonatiuh.martinez@globant.com>
2016-10-11 19:06:30 -0500

    Reducer for accountNumber.

---
+++ js/reducers/transfers/AccountNumber.js
@@ -0,0 +1,37 @@
+import Immutable from 'immutable';
+
+const initialState = Immutable.Map (
+    {
+        AccountNumber: {
+            number: '000000000000',
+            name: 'accountRemitted',
+            fieldName: 'Account to be remitted',
+            htmlForId: 'accountRemitted'
+        }
+    }
+);
+
+const AccountNumber = (state = initialState, action) => {
+    console.log("reducer", action);
+    switch(action.type) {
+        case 'CHANGE_ACCOUNT_NUMBER':
+            //console.log("can", action.data);
+            return state.merge(
+                {
+                    accountNumber:{number: action.data.value}
+                }
+            );
+        case 'REJECT_VALUE':
+            //console.log("rv", state);
+            return state.merge(
+                {
+                    accountNumber: action.data.value
+                }
+            );
+        default:
+            //console.log("def", state, action.data);
+            return state;
+    }
+};
+
+module.exports = AccountNumber;
