c7b2530cfa02d3ad3e820dd1df652ad4c08c8849
choco <tonatiuh.martinez@globant.com>
2016-10-11 19:03:52 -0500

    Success Message trial

---
+++ js/components/common/SuccessMessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SuccessMessage extends React.Component {
+    render() {
+        //let 
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SuccessMessage;
