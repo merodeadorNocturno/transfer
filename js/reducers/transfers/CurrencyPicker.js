df780083d2db299d0c3d7eb43c6987956efed9fa
choco <tonatiuh.martinez@globant.com>
2016-10-11 19:07:44 -0500

    Reducer for currency picker.

--- js/reducers/transfers/CurrencyPicker.js
+++ js/reducers/transfers/CurrencyPicker.js
@@ -1,18 +1,16 @@
 import Immutable from 'immutable';
 
 const initialState = Immutable.Map({
-    accountToBeRemitted: '000000000000',
-    remittanceAccount: '000000000000',
     selectedCurrency: 'USD'
 });
 
 const CurrencyPicker = (state = initialState, action) => {
     switch(action.type) {
         case "CHANGE_VALUE":
-            console.log("action", state, action);
+            //console.log("action", state, action);
             return state.merge({selectedCurrency: action.data.value});
         default:
-            console.log("default", state, action);
+            //console.log("default", state, action);
             return state;
 
     }
