8ff9a284cf24a4f290ba8e6f3dad22544cea963f
choco <tonatiuh.martinez@globant.com>
2016-10-13 17:20:48 -0500

    Changed invocation method
    Since TransferForm is now using a reducer, we have to change our invocation method to { TransferForm }

--- js/components/common/MainBank.js
+++ js/components/common/MainBank.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TransferForm from '../transfers/TransferForm';
+import { TransferForm } from '../transfers/TransferForm';
 import Scheduler from '../transfers/Scheduler';
 import NavigationBar from './NavigationBar';
 
