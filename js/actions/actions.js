ba7cd1ea169ed0ea24a0a15f585114156f08ed2a
choco <tonatiuh.martinez@globant.com>
2016-10-14 16:40:08 -0500

    Modified tons of code for async actions.js

--- js/actions/actions.js
+++ js/actions/actions.js
@@ -1,20 +1,62 @@
 import Immutable from 'immutable';
 
-export const INSERT_RECORD = 'INSERT_RECORD';
-export const RETRIEVE_RECORD = 'RETRIEVE_RECORD';
-export const RETRIEVE_ALL_RECORDS = 'RETRIEVE_ALL_RECORDS';
+export const REQUEST_TRANSFERS = 'REQUEST_TRANSFERS';
+export const RECEIVE_TRANSFERS = 'RECEIVE_TRANSFERS';
+export const INSERT_TRANSFER = 'INSERT_TRANSFER';
+export const INVALIDATE_TRANSFER = 'INVALIDATE_TRANSFER';
 
 
-export const insertRecord = (transfer) => {
+export const insertTransfer = (transfer) => ({
+    type: INSERT_TRANSFER,
+    transfer
+});
 
+export const invalidateTransfer = (transfer) => ({
+    type: INVALIDATE_TRANSFER,
+    transfer
+});
+
+export const requestTransfers = (transfer) => {
+    return { type: RETRIEVE_RECORD, transfer };
 };
 
-/**
- * Retrieves all records from DB
- * @returns {Object|null} Transfers object
- */
-const retrieveAllRecords = () => {
-    //setTimeout(() => {
+
+export const receiveTransfers = (transfer, json) => {
+    console.log(json);
+    return {
+        type: RETRIEVE_ALL_RECORDS,
+        transfer,
+        posts: json.data.children.map(child => child.data),
+        receivedAt: Date.now()
+     };
+};
+
+const fetchTransfers = (transfer) => (dispatch) => {
+    dispatch(requestTransfers(transfer));
+
+    return new Promise((resolve, reject) => {
+        const json = getFromLocalStorage();
+        if (json !== null) {
+            resolve(JSON.parse(json));
+        } else {
+            reject('No data available...');
+        }
+    }).then((json) => {
+        dispatch(receiveTransfers(transfer, json));
+    }).catch((reason) => {
+        return console.log('No data available');
+    });
+};
+
+
+
+
+export const invalidateTransaction = (transfer) => ({ type: INVALIDATE_TRANSACTION, transfer });
+
+export const fetchAllRecords = (transfer) => (dispatch) => {
+    dispatch(retrieveAllRecords);
+};
+
+const getFromLocalStorage = () => {
     return localStorage.getItem('transfers');
-    //}, 1500);
 };
