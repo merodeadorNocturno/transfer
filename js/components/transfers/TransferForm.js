91da08934a1e12bd6d1d4ef681b391c90fad01dd
choco <tonatiuh.martinez@globant.com>
2016-10-14 19:08:28 -0500

    Refactor of TransferForm
    Now with state and reducers

--- js/components/transfers/TransferForm.js
+++ js/components/transfers/TransferForm.js
@@ -1,47 +1,53 @@
 require('./TransferForm.scss');
 import React from 'react';
+import FormData from 'react-form-data';
 import CurrencyInput from 'react-currency-input';
 import { CurrencyPicker } from '../common/CurrencyPicker';
 import { AccountNumber } from '../common/AccountNumber';
 import { Scheduler } from './Scheduler';
+import { connect } from 'react-redux';
 
 
 class TransferForm extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            amount: "0.00"
-        };
-        this.handleChange = this.handleChange.bind(this);
+
+    handleSubmit(event) {
+        event.preventDefault();
     }
 
-    handleChange (value) {
-        this.setState({amount: value});
-    }
-
-    render () {
+    render() {
+        const {TransferForm} = this.props;
+        console.log('trans', TransferForm);
         return (
-            <form className="form">
+            <form className="form" onSubmit={this.handleSubmit}>
                 <AccountNumber
                     htmlForId="accountRemitted"
                     label="Origin Account"
-                />
+                    onChange={this.props.onChangeOriginAccount}
+                    />
                 <AccountNumber
                     htmlForId="remittanceAccount"
                     label="Recipient Account"
-                />
+                    onChange={this.props.onChangeRecipientAccount}
+                    />
                 <CurrencyPicker />
                 <div className="form-group">
                     <span>
-                    <label>
-                        Schedule this operation
-                    </label>
+                        <label>
+                            Schedule this operation!!
+                        </label>
                     </span>
                     <span>
-                        <input type="checkbox" id="checkboxSchedule" className="form__checkbox"/>
+                        <input 
+                            type="checkbox" 
+                            id="checkboxSchedule" 
+                            className="form__checkbox" 
+                            onChange={this.props.onChangeSchedulerOn} />
                     </span>
                 </div>
-                <Scheduler />
+                <Scheduler
+                    onChange={this.props.onChangeScheduler}
+                    schedulerClassName={TransferForm.get('schedulerHidden')}
+                 />
                 <div className="form-group">
                     <button type="submit" className="btn btn-default">Submit</button>
                 </div>
@@ -50,14 +56,36 @@ class TransferForm extends React.Component {
     }
 }
 
-const validateAccount = (account) => {
-    let accountMatch = [];
-    accountMatch = (account + '').match(/^[0-9]{12}$/);
-    if (accountMatch.length) {
 
+module.exports.TransferForm = connect(
+    (state) => {
+        return { TransferForm: state.TransferForm };
+    },
+    {
+        onChangeOriginAccount: (data) => { console.log('enter', data);
+            return { type: 'CHANGE_ORIGIN_ACCOUNT', data };
+        },
+        onChangeRecipientAccount: (data) => {
+            return { type: 'CHANGE_RECIPIENT_ACCOUNT', data };
+        },
+        onChangeSelectCurrency: (data) => {
+            return { type: 'CHANGE_SELECT_CURRENCY', data };
+        },
+        onChangeAmount: (data) => {
+            return { type: 'CHANGE_AMOUNT', data };
+        },
+        onChangeSchedulerOn: (data) => {
+            return { type: 'CHANGE_SCHEDULER', data };
+        },
+        onChangeInitialDate: (data) => {
+            return { type: 'CHANGE_INITIAL_DATE', data };
+        },
+        onChangeFinalDate: (data) => {
+            return { type: 'CHANGE_FINAL_DATE', data };
+        },
+        onChangePeriodicity: (data) => {
+            return { type: 'CHANGE_PERIODICITY', data };
+        }
     }
-};
+)(TransferForm);
 
-//console.log(store);
-
-export default TransferForm;
