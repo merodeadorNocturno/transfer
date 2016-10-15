0a2793b38592bc2807f42113e3cc0a517188756d
choco <tonatiuh.martinez@globant.com>
2016-10-14 19:08:46 -0500

    New Reducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -3,19 +3,20 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 // @ reducers
-import transfers from './transfers/makeTransfer';
 import CurrencyPicker from './transfers/CurrencyPicker';
 import AccountNumber from './transfers/AccountNumber';
 import Scheduler from './transfers/Scheduler';
 import Amount from './transfers/Amount';
+import TransferForm from './transfers/TransferForm';
+import Transfers from './reducers';
 
 
 const appReducer = combineReducers({
-    transfers,
     CurrencyPicker,
     AccountNumber,
     Scheduler,
     Amount,
+    TransferForm,
     routing: routerReducer
 });
 
